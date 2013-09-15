2c27c9e7bab30e477fee654e5c02d198a86024f8
Bin Long <binlong.plus@gmail.com>
2013-09-16 00:15:53 +0800

    Binary Tree Level Order Traversal

--- leetcode/BinaryTreeLevelOrderTraversal.cpp
+++ leetcode/BinaryTreeLevelOrderTraversal.cpp
@@ -1,21 +1,21 @@
 vector< vector<int> > levelOrderII( TreeNode* root )
 {
-	vector<vector<int>> vc;
+    vector<vector<int>> vc;
     if( root==NULL )
-	{
-		return vc;
-	}
-	vector<int> cur;
+    {
+        return vc;
+    }
+    vector<int> cur;
     cur.push_back(root->val);
     vector<TreeNode*> curNodes;
     curNodes.push_back(root);
-	while( cur.size()!=0 )
-	{
+    while( cur.size()!=0 )
+    {
         vc.push_back( cur );
         vector<TreeNode*> parents = curNodes;
         curNodes.clear();
         cur.clear();
-		for( int i = 0; i != parents.size(); ++i )
+        for( int i = 0; i != parents.size(); ++i )
         {
             if(parents[i]->left)
             {
@@ -28,5 +28,5 @@ vector< vector<int> > levelOrderII( TreeNode* root )
                 cur.push_back( parents[i]->right->val );   
             }
         }
-	}
+    }
 }
