d7b104a0e215428036ef8491081ad6ddc80bf4ae
靖难 <stackpop2013@gmail.com>
2013-09-25 00:11:58 +0800

    Update WordLadder2.cpp

--- leetcode/WordLadder2.cpp
+++ leetcode/WordLadder2.cpp
@@ -3,21 +3,26 @@ public:
     static vector< vector<string> > findLadders( string start, string end, unordered_set<string> & dict ) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        vector<string> cur_layer;
-        vector<string> pre_layer;
-        map< string, vector<string> > parents;
-        cur_layer.push_back(start);
-        dict.erase( start );
+        vector< vector<string> > result;
+        unordered_set<string> cur_layer;
+        unordered_set<string> pre_layer;
+        unordered_map< string, vector<string> > parents;
+        cur_layer.insert(start);
         dict.insert( end );
+        dict.erase(start);
+        bool findIt = false;
         while( cur_layer.size() != 0 )
         {
-        	pre_layer = cur_layer;
-        	cur_layer.clear();
-        	for( int i = 0; i != pre_layer.size(); ++i )
+            pre_layer = cur_layer;
+            cur_layer.clear();
+        	for( unordered_set<string>::iterator it = pre_layer.begin(); 
+                it != pre_layer.end(); ++it )
         	{
-        		string cur_str( pre_layer[i] );
+        	    string cur_str = *it ;
         		if( cur_str==end )
         		{
+        		    findIt = true;
+        		    cur_layer.clear();
         			break;
         		}
         		for( int j = 0; j != cur_str.size(); ++j )
@@ -33,35 +38,41 @@ public:
 	        			unordered_set<string>::iterator it = dict.find( tmp_str );
 	        			if( it != dict.end() )
 	        			{
-	        				cur_layer.push_back( tmp_str );
+	        				cur_layer.insert( tmp_str );
 	        				parents[tmp_str].push_back(cur_str);
 	        			}
 	        		}
         		}
         	}
-        	for( int i = 0; i != cur_layer.size(); ++i )
+        	for( unordered_set<string>::iterator it = cur_layer.begin(); 
+                it != cur_layer.end(); ++it  )
         	{
-        		dict.erase(cur_layer[i]);
+        		dict.erase(*it);
         	}
         }
+        if( !findIt )
+            return result;
         //cout<<parents[end][0]<<endl;
-        vector< vector<string> > result;
         vector< string > path;
         buildPath( parents, result, path, end );
         return result;
     }
-    static void buildPath( map<string, vector<string> >&parents, vector< vector<string> >& result, vector<string> path, string cur )
+    static void buildPath( unordered_map<string, vector<string> >&parents, vector< vector<string> >& result, vector<string> &path, string &cur )
     {
-    	if( parents.find(cur) == parents.end() )
+    	if( parents[cur].size()==0 )
     	{
-    		result.push_back( path );
+    	    path.push_back(cur);
+    	    vector<string> revPath = path;
+    	    reverse( revPath.begin(), revPath.end() );
+    		result.push_back( revPath );
+    		path.pop_back();
     		return;
     	}
+    	path.push_back(cur);
     	for( int i = 0; i != parents[cur].size(); ++i )
     	{
-    		//cout<<cur<<"<-"<<parents[cur][i]<<endl;
     		buildPath( parents, result, path, parents[cur][i] );
-    		path.push_back( parents[cur][i]);
     	}
+    	path.pop_back();
     }
 };
