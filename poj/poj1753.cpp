bfbef063d6e048935280ab1c13e372f6e7e8fe5e
靖难 <stackpop2013@gmail.com>
2013-10-02 12:23:44 +0800

    add solution to poj1753

--- poj/poj1753.cpp
+++ poj/poj1753.cpp
@@ -1,57 +1,59 @@
-/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-
-#include<iostream>
-#include<vector>
-#include<cstdlib>
+#include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
 
-void dfs( vector<int> queen, int n, int step, int& result )
-{
-    if(step==n)
-    {
-        result = (result+1)%1000000007;
-        return;
-    }
-    for( int i = 0; i != n; ++i )
-    {
-        bool valid = true;
-        for( int j = 0; j != step; ++j )
-        {
-            if( i == queen[j] )
-            {
-                valid = false;
-                break;
+int flip( int cur, int i, int j ){
+    cur = cur^( 1<<(i*4+j) );
+    if( i-1 >= 0 ) cur = cur^(1<<(4*(i-1)+j));
+    if( i+1 < 4 ) cur = cur^(1<<(4*(i+1)+j));
+    if( j-1 >= 0 ) cur = cur^(1<<(4*i+j-1));
+    if( j+1 <4 ) cur = cur = cur^(1<<(4*i+j+1));
+    return cur;
+}
+
+int bfs( int from ){
+    int result[65536];
+    int vis[65536];
+    queue<int> Q;
+    memset( result, 0x0, sizeof(int)*65536 );
+    memset( vis, 0x0, sizeof(int)*65536 );
+    Q.push( from );
+    vis[from] = 1;
+    while( !Q.empty() ){
+        int cur = Q.front();
+        Q.pop();
+        if( cur==0 || cur==65535 ){
+            return result[cur];
+        }
+        for( int i = 0; i != 4; ++i ){
+            for( int j = 0; j != 4; ++j ){
+                int state = flip( cur, i, j );
+                if( vis[state]==0 ){
+                    Q.push( state );
+                    vis[state] = 1;
+                    result[state] = result[cur] + 1;
+                }
             }
         }
-        if( abs(queen[step-1]-i)<=1 )
-        {
-            valid = false;
-        }
-        if(valid)
-        {
-            queen[step] = i;
-            dfs(queen, n, step+1, result);
-        }
     }
+    return -1;
 }
 
 int main()
 {
-    vector<int> queen(20);
-    int testCaseNum;
-    cin>>testCaseNum;
-    for( int i = 0; i != testCaseNum; ++i )
-    {
-        int N,k;
-        cin>>N>>k;
-        for( int i = 0; i != k; ++i )
-        {
-            int data;
-            cin>>data;
-            queen[i] = data;
+    int from = 0;
+    for( int i = 0; i != 4; ++i ){
+        for( int j = 0; j != 4; ++j ){
+            char c;
+            cin>>c;
+            from =  from + ((c=='w')?0:1)*( 1<<(i*4+j) );
         }
-        int result = 0;
-        dfs( queen, N, k, result );
+    }
+    int result = bfs( from );
+    if( result==-1 ){
+        cout<<"Impossible"<<endl;
+    }else{
         cout<<result<<endl;
     }
     return 0;
