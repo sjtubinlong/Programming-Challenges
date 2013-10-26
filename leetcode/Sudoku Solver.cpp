088695c7cf81f312144abd4f32af1320b415ee79
靖难 <stackpop2013@gmail.com>
2013-10-26 23:50:30 +0800

    Valid solution with WA

--- leetcode/Sudoku Solver.cpp
+++ leetcode/Sudoku Solver.cpp
@@ -5,31 +5,33 @@ public:
         vector<int> row(10, 0);
         vector<int> col(10, 0);
         vector<int> block(10, 0);
-        vector<int> needFill;
+        vector<int> toFill;
         for( int i = 0; i < 9; ++i ){
             for( int j = 0; j < 9; ++j ){
-                if( board[i][j] != '.' ) {
+                if( board[i][j] !='.' ){
                     int c = board[i][j]-'0';
                     row[i] |= (1<<c);
                     col[j] |= (1<<c);
                     block[i/3*3+j/3] |= (1<<c);
                 }else{
-                    needFill.push_back( i*9 + j );
+                    toFill.push_back( i*9 +j );
                 }
             }
         }
-        dfs( board, row, col, block, needFill, 0 );
+        dfs( board, row, col, block, toFill, 0 );
     }
-    bool dfs( vector<vector<char> > &board, vector<int>&row, vector<int>&col, vector<int>&block,vector<int>& needFill, int k ){
-        if( k >= needFill.size() ) return true;
-        int x = needFill[k]/9, y = needFill[k]%9;
+    bool dfs(  vector<vector<char> > &board, vector<int>&row, vector<int>&col, vector<int>&block, vector<int>&toFill, int k ){
+        if( k >= toFill.size() ){
+            return true;
+        }
+        int x = toFill[k]/9, y = toFill[k]%9;
         for( int i = 1; i <= 9; ++i ){
-            if( row[x]&(1<<i) || col[y]&(1<<i) || block[x/3*3+y/3]&(1<<i) ) continue;
+            if( row[x]&(1<<i) || col[y]&(1<<i) || block[x*3/3+y/3]&(1<<i) ) continue;
             board[x][y] = i + '0';
             row[x] |= (1<<i);
             col[y] |= (1<<i);
             block[x/3*3+y/3] |= (1<<i);
-            bool ret = dfs( board, row, col, block, needFill, k+1 );
+            bool ret = dfs( board, row, col, block, toFill, k+1 );
             if( ret ) return true;
             row[x] ^= (1<<i);
             col[y] ^= (1<<i);
@@ -37,5 +39,5 @@ public:
         }
         board[x][y] = '.';
         return false;
-    } 
+    }
 };
