23b3aa1396c983359a25f56ca23eac9c2b7bdf5d
靖难 <stackpop2013@gmail.com>
2013-10-29 22:26:52 +0800

    Linked List Cycle

--- leetcode/Linked List Cycle.cpp
+++ leetcode/Linked List Cycle.cpp
@@ -5,13 +5,12 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- */
+ */ 
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        if( head==NULL || head->next==NULL ) return false;
         ListNode* slow = head;
         ListNode* fast = head;
         do{
