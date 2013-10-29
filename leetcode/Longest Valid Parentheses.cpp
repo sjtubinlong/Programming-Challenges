da1382540c6fd237045cb64900ca1295a1914dc2
靖难 <stackpop2013@gmail.com>
2013-10-29 22:09:16 +0800

    Longest Valid Parentheses

--- leetcode/Longest Valid Parentheses.cpp
+++ leetcode/Longest Valid Parentheses.cpp
@@ -4,15 +4,12 @@ public:
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
         int n = s.size();
-        vector<int> match( n, -1 );
         stack<int> stk;
         for( int i = 0; i < n; ++i ) {
             if( s[i] == '(' ){
                 stk.push(i);
             }else{
-                if( stk.empty() ){
-                    match[i] = -1;
-                }else{
+                if( !stk.empty() ){
                     s[i] = '.';
                     s[ stk.top() ] = '.';
                     stk.pop();   
