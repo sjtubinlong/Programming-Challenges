e0cd6585ccc878d00ac6bff2a2a134ae5ea784b1
靖难 <stackpop2013@gmail.com>
2013-11-04 23:57:21 +0800

    Rotate Image

---
+++ leetcode/Rotate Image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        int n = matrix.size();
+        for( int i = 0; i < n/2; ++i ){
+            for( int j = i; j < n-1-i; ++j ){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            } 
+        }
+    }
+};
