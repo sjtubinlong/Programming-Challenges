50df0c6964214d8028c7b473d2f5b9642f9ade5c
binlong <binlong.plus@gmail.com>
2015-04-07 23:55:44 +0800

    binary search in leetcode

---
+++ leetcode/Search_Insert_Position/SearchInsertPosition.cpp
@@ -0,0 +1,9 @@
+int searchInsert(int A[], int n, int target) {
+    int low = 0, high = n - 1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(A[mid] >= target) high = mid - 1;
+        if(A[mid] < target) low = mid + 1;
+    }
+    return low;
+}
