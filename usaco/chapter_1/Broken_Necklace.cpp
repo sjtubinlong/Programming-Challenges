d94d33dcd8afffa30b7d28de060fb4bc3edddb15
binlong <binlong.plus@gmail.com>
2015-04-15 23:15:28 +0800

    optimize codes

--- usaco/chapter_1/Broken_Necklace.cpp
+++ usaco/chapter_1/Broken_Necklace.cpp
@@ -10,37 +10,41 @@ LANG: C++
 # include <string>
 using namespace std;
 
-int solve(string& beads){
-    beads += beads;
+int solve(const string& beads){
     int n = beads.size();
     int ans = 0;
     int i = 0;
     while(i < n) {
-        while(beads[i] == 'w') ++i;
-         int n1 = 0;
-         int j = i - 1;
-         while(j >= 0 && beads[j] == 'w'){
-            ++n1;
-            --j;
-         }
-         int k = j;
-         while(k >= 0){
-            if(beads[k] == beads[j] || beads[k] == 'w') ++n1;
-            else break;
-            --k;
-         }
+        while(beads[i] == 'w' && i < n) ++i;
+        if(i == n){
+            if(ans == 0) ans = n;
+            break;
+        }
+        int n1 = 0;
+        int j = (i - 1 + n) % n;
+        while(j != i && beads[j] == 'w'){
+           ++n1;
+           j = (j - 1 + n) % n;
+        }
+        int k = j;
+        while(k != i){
+           if(beads[k] == beads[j] || beads[k] == 'w') ++n1;
+           else break;
+           k = (k - 1 + n) % n;
+        }
+        k = (k + 1) % n;
 
-         int n2 = 0;
-         j = i;
-         while(j < n) {
-            if(beads[j] == beads[i] || beads[j] == 'w') ++n2;
-            else break;
-            ++j;
-         }
-         if(n1 + n2 > ans) ans = n1 + n2;
-         ++i;
+        int n2 = 0;
+        j = i;
+        while(j != k) {
+           if(beads[j] == beads[i] || beads[j] == 'w') ++n2;
+           else break;
+           j = (j + 1) % n;
+        }
+        if(n1 + n2 > ans) ans = n1 + n2;
+        ++i;
     }
-    return min(ans, n / 2);
+    return ans;
 }
 
 
--- usaco/chapter_1/Broken_Necklace_brute.cpp
+++ usaco/chapter_1/Broken_Necklace_brute.cpp
@@ -16,7 +16,7 @@ int solve_without_mod(string& beads){
     int ans = 0;
     int i = 0;
     while(i < n) {
-        while(beads[i] == 'w') ++i;
+        while(i < n && beads[i] == 'w') ++i;
          int n1 = 0;
          int j = i - 1;
          while(j >= 0 && beads[j] == 'w'){
@@ -42,51 +42,43 @@ int solve_without_mod(string& beads){
     }
     return min(ans, n / 2);
 }
-int solve_with_mod(string& beads){
+
+int solve_with_mod(const string& beads){
     int n = beads.size();
     int ans = 0;
-    for(int i = 0; i < n; ++i){
-        int o1 = 0;
-        int c = -1;
+    int i = 0;
+    while(i < n) {
+        while(beads[i] == 'w' && i < n) ++i;
+        if(i == n){
+            if(ans == 0) ans = n;
+            break;
+        }
+        int n1 = 0;
         int j = (i - 1 + n) % n;
         while(j != i && beads[j] == 'w'){
-            ++o1;
-            j = (j - 1 + n) % n;
+           ++n1;
+           j = (j - 1 + n) % n;
         }
-        if(j != i) c = beads[j];
-        else{
-            return n;
+        int k = j;
+        while(k != i){
+           if(beads[k] == beads[j] || beads[k] == 'w') ++n1;
+           else break;
+           k = (k - 1 + n) % n;
         }
-        while(j != i){
-            if(beads[j] != c && beads[j] != 'w') break;
-            o1 += 1;
-            j = (j - 1 + n) % n;
+        k = (k + 1) % n;
+
+        int n2 = 0;
+        j = i;
+        while(j != k) {
+           if(beads[j] == beads[i] || beads[j] == 'w') ++n2;
+           else break;
+           j = (j + 1) % n;
         }
-        if(j == i) return n;
-        int o2 = 0;
-        c = -1;
-        int k = i;
-        while(k != j && beads[k] == 'w'){
-            ++o2;
-            k = (k + 1) % n;
-        }
-        if(k != j) c = beads[j];
-        else{
-            return n;
-        }
-        while(k != j){
-            if(beads[k] != c && beads[k] != 'w') break;
-            o2 += 1;
-            k = (k + 1) % n;
-        }
-        if(k == j){
-            if(beads[j] == c) o2 += 1;
-        }
-        if(o1 + o2 > ans) ans = o1 + o2;
+        if(n1 + n2 > ans) ans = n1 + n2;
+        ++i;
     }
     return ans;
 }
-
 
 int main(){
     ofstream fout ("beads.out");
